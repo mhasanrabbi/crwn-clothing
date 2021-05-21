75533dbd00b70698fedd13c2ad2b05ca3ac8bf8f
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-05-21 20:26:16 +0600

    redux set up

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
+import store from './redux/store';
+
 import App from './App';
+
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
